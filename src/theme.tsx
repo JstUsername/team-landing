35dd11663e4e6f564d4b7fe87d4df1f584f8a41a
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-01 21:33:41 +0300

    feat(theme.tsx): добавлена тема

---
+++ src/theme.tsx
@@ -0,0 +1,88 @@
+import '@fontsource/roboto';
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#458FF6',
+    },
+    secondary: {
+      main: '#A2A9B0',
+      light: '#F2F4F8',
+      dark: '#697077',
+    },
+    background: {
+      paper: '#F2F4F8',
+    },
+    text: {
+      primary: '#21272A',
+      secondary: '#697077',
+    },
+    info: {
+      main: '#458FF6',
+    },
+    success: {
+      main: '#25A249',
+    },
+    divider: '#DDE1E6',
+  },
+  typography: {
+    h1: {
+      fontSize: 54,
+      lineHeight: 1.1,
+      letterSpacing: '0em',
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: 42,
+      fontWeight: 700,
+      lineHeight: 1.1,
+      letterSpacing: '0em',
+    },
+    button: {
+      fontSize: 20,
+      lineHeight: 1,
+      letterSpacing: '0.03em',
+    },
+    body1: {
+      fontSize: 18,
+      lineHeight: 1.4,
+      letterSpacing: '0em',
+    },
+    body2: {
+      fontSize: 16,
+      lineHeight: 1.4,
+      letterSpacing: '0em',
+    },
+    h3: {
+      fontSize: 32,
+      fontWeight: 700,
+      lineHeight: 1.1,
+    },
+    h4: {
+      fontSize: 24,
+      fontWeight: 700,
+      lineHeight: 1.1,
+      letterSpacing: '0em',
+    },
+    h5: {
+      fontSize: 20,
+      fontWeight: 700,
+      lineHeight: 1.1,
+    },
+    subtitle1: {
+      fontSize: 18,
+      lineHeight: 1.4,
+      letterSpacing: '0em',
+    },
+    subtitle2: {
+      fontSize: 16,
+      fontWeight: 400,
+      lineHeight: 1.4,
+      letterSpacing: '0em',
+    },
+  },
+});
+
+export default theme;
