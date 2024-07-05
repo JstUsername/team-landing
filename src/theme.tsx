181ab138aadcf52d4c5e02733845d5c513be028c
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-05 17:12:23 +0300

    fix(xs): значение для xs брейкпоинта изменено на 0

--- src/theme.tsx
+++ src/theme.tsx
@@ -4,7 +4,7 @@ import { createTheme } from '@mui/material/styles';
 export const theme = createTheme({
   breakpoints: {
     values: {
-      xs: 360,
+      xs: 0,
       sm: 768,
       md: 1024,
       lg: 1440,
