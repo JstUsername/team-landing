3f7781c9a3e322b08723858a037b372e8023b629
vabulaeva <veronikabylaeva99@gmail.com>
2024-07-05 14:26:26 +0300

    fix(knowledge): Поправлены комментарии

--- src/commons/button/DefaultButton.tsx
+++ src/commons/button/DefaultButton.tsx
@@ -1,18 +1,18 @@
 import { FC } from 'react';
-import { styled } from '@mui/material';
+import { ButtonProps, styled } from '@mui/material';
 import MuiButton from '@mui/material/Button';
 import { IDefaultButton } from './types';
 import { theme } from '../../theme';
 
-const DefaultButton: FC<IDefaultButton> = ({ type, text }) => {
+const DefaultButton: FC<IDefaultButton> = ({ type, text, fontSizeSx }) => {
   return (
-    <Button variant={type} color={'info'}>
+    <Button variant={type} color={'info'} fontSizeSx={fontSizeSx}>
       {text}
     </Button>
   );
 };
 
-const Button = styled(MuiButton)({
+const Button = styled(MuiButton)((props: { fontSizeSx: number } & ButtonProps) => ({
   'paddingInline': 32,
   'paddingBlock': 16,
   'border': ['2', 'solid'],
@@ -21,11 +21,14 @@ const Button = styled(MuiButton)({
   'boxShadow': 'none',
   'textTransform': 'none',
   ':hover': {
-    paddingInline: 28,
-    paddingBlock: 14,
     border: ['2', 'solid'],
     borderColor: '#3064ac',
   },
-});
+  '@media (max-width: 768px)': {
+    paddingInline: 28,
+    paddingBlock: 14,
+    fontSize: props.fontSizeSx,
+  },
+}));
 
 export default DefaultButton;
