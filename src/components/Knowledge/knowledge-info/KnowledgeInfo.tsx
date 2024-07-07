621bc07cb66c598a382249775ee6a4cef9e59d3b
vabulaeva <veronikabylaeva99@gmail.com>
2024-07-07 20:36:01 +0300

    fix(knowledge):
    Добавлен svgr, исправлено написание стилей, исправлены комментарии

--- src/components/Knowledge/knowledge-info/KnowledgeInfo.tsx
+++ src/components/Knowledge/knowledge-info/KnowledgeInfo.tsx
@@ -1,37 +1,42 @@
 import { FC } from 'react';
-import { Box, Grid, styled, Typography } from '@mui/material';
+import { Box, styled, Typography } from '@mui/material';
 import { IKnowledgeInfo } from '../types';
 
 const KnowledgeInfo: FC<Omit<IKnowledgeInfo, 'id'>> = ({ image, description, statistics }) => {
   return (
-    <Grid item xs={6}>
-      <Box border={1} borderColor={'divider'} borderRadius={2} p={2} minHeight={{ xs: 168, sm: 0 }}>
-        <GridContainer>
-          <Box alignItems={'center'} maxHeight={48}>
-            <img src={image} alt="иконка" width={48} height={48} />
-          </Box>
-          <Box>
-            <Typography variant={'h4'} textAlign={{ xs: 'center', lg: 'left' }}>
-              {statistics}
-            </Typography>
-            <Typography variant={'subtitle2'} color={'secondary.dark'} textAlign={{ xs: 'center', lg: 'left' }}>
-              {description}
-            </Typography>
-          </Box>
-        </GridContainer>
-      </Box>
-    </Grid>
+    <Box border={1} borderColor={'divider'} borderRadius={2} p={{ xs: 1, md: 2 }} minHeight={{ xs: 168, sm: 0 }}>
+      <GridContainer>
+        <Box alignItems={'center'} maxHeight={48}>
+          {image}
+        </Box>
+        <Box>
+          <Typography variant={'h4'} textAlign={{ xs: 'center', lg: 'left' }}>
+            {statistics}
+          </Typography>
+          <Typography
+            variant={'subtitle2'}
+            fontSize={{ xs: 14, lg: 16 }}
+            color={'secondary.dark'}
+            textAlign={{ xs: 'center', lg: 'left' }}
+          >
+            {description}
+          </Typography>
+        </Box>
+      </GridContainer>
+    </Box>
   );
 };
 
-const GridContainer = styled('div')({
-  'display': 'flex',
-  'flexDirection': 'row',
-  'alignItems': 'flex-start',
-  'gap': 16,
-  '@media (max-width: 768px)': {
-    flexDirection: 'column',
-    alignItems: 'center',
+const GridContainer = styled('div')`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 16px;
+  height: 100%;
+  @media (max-width: ${({ theme }) => theme.breakpoints.values.sm}px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
   },
-});
+`;
 export default KnowledgeInfo;
