a9378f5cdebd2fdb79b5abe7549c7c4c1e8efe7e
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-08 21:06:16 +0300

    feat(validation): добавлена валидация email и пароля.
    Email валидируется на входе и регистрации, пароль валидируеся только на регистрации.

--- src/commons/FormPasswordInput/FormPasswordInput.tsx
+++ src/commons/FormPasswordInput/FormPasswordInput.tsx
@@ -3,10 +3,33 @@ import EyeIcon from '../../assets/login/icons/eye.svg?react';
 import EyeCloseIcon from '../../assets/login/icons/eye-close.svg?react';
 import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
-import { useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 
-export default function FormPasswordInput() {
+interface FormPasswordInputProps {
+  typeLogin: string;
+}
+
+export default function FormPasswordInput({ typeLogin }: FormPasswordInputProps) {
+  const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(false);
+
+  const validatePassword = (password: string) => {
+    const re = /^(?=.*[a-z])(?=.*[A-Z0-9]).{8,}$/;
+    return re.test(String(password));
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  useEffect(() => {
+    if (password === '') {
+      setError(false);
+    } else {
+      validatePassword(password) ? setError(false) : setError(true);
+    }
+  }, [password]);
 
   return (
     <FormInput
@@ -14,6 +37,8 @@ export default function FormPasswordInput() {
       inputLabel="Пароль"
       inputPlaceholder="******"
       type={showPassword ? 'text' : 'password'}
+      inputError={typeLogin === 'registration' ? error : undefined}
+      inputOnChange={typeLogin === 'registration' ? handleChange : undefined}
       InputProps={{
         endAdornment: (
           <InputAdornment position="end">
