08edb5654d177e0ef66fbc7b835fb9807dc22159
vabulaeva <veronikabylaeva99@gmail.com>
2024-07-08 12:33:17 +0300

    fix(knowledge): Поправлены комментарии

--- src/components/Knowledge/Knowledge.tsx
+++ src/components/Knowledge/Knowledge.tsx
@@ -1,33 +1,31 @@
 import { FC } from 'react';
 import { Box, Container, styled, Typography } from '@mui/material';
-import MuiTypography from '@mui/material/Typography';
 import KnowledgeInfo from './KnowledgeInfo';
 import { knowledgeInfo } from './constants';
 import Chart from './chart/Chart';
-import theme from '../../theme';
 import ButtonLink from '../../commons/button/ButtonLink';
 
 const Knowledge: FC = () => {
   return (
-    <Container maxWidth={'lg'} sx={{ background: 'background.default', marginBlock: { xs: 5, sm: 10 } }}>
+    <Container maxWidth="lg" sx={{ background: 'background.default', marginBlock: { xs: 5, sm: 10 } }}>
       <GridContainer>
         <Box>
-          <Typography variant={'h5'} color={'primary.dark'} mb={1} textTransform={'uppercase'}>
+          <Typography variant="h5" color="primary.dark" mb={1} textTransform="uppercase">
             знания
           </Typography>
-          <KnowledgeText color={'text.primary'} align={'left'}>
+          <Typography variant="h4" color="text.primary" align="left" fontSize={{ xs: 24, lg: 42 }}>
             Информацию по отдельным заболеваниям ориентированную на пациентов можно найти в соответствующем разделе
             сайта, статьи разделены на категории.
-          </KnowledgeText>
+          </Typography>
         </Box>
         <FlexButton>
-          <ButtonLink variant={'contained'}>
-            <Typography variant={'button'} fontSize={{ xs: 14, lg: 20 }} textAlign={'center'} textTransform={'none'}>
+          <ButtonLink variant="contained">
+            <Typography variant="button" fontSize={{ xs: 14, lg: 20 }} textAlign="center" textTransform="none">
               Для врачей
             </Typography>
           </ButtonLink>
-          <ButtonLink variant={'outlined'}>
-            <Typography variant={'button'} fontSize={{ xs: 14, lg: 20 }} textAlign={'center'} textTransform={'none'}>
+          <ButtonLink variant="outlined">
+            <Typography variant="button" fontSize={{ xs: 14, lg: 20 }} textAlign="center" textTransform="none">
               Для пациентов
             </Typography>
           </ButtonLink>
@@ -59,8 +57,8 @@ const GridContainer = styled('div')`
   flex-direction: column;
   gap: 48px;
   margin-inline: 80px;
-  
-  ${theme.breakpoints.down('sm')} {  
+
+  ${({ theme }) => theme.breakpoints.down('sm')}  {  
     margin-inline: 0;  
   },`;
 
@@ -69,33 +67,23 @@ const FlexButton = styled('div')`
   gap: 16px;
 `;
 
-const KnowledgeText = styled(MuiTypography)`
-  font-size: 42px;
-  font-weight: 700;
-  letter-spacing: 0;
-  
-  ${theme.breakpoints.down('sm')} {
-    font-size: 24px;
-  },
-`;
-
 const FlexKnowledgeItem = styled('div')`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 80px;
 
-  ${theme.breakpoints.down('lg')} {
+  ${({ theme }) => theme.breakpoints.down('lg')}  {
     grid-template-columns: 100%;
     gap: 24px;
   },
 `;
 
 const KnowledgeItemBox = styled('div')`
-  border: 1px solid ${theme.palette.divider};
+  border: 1px solid ${({ theme }) => theme.palette.divider};
   height: 396px;
   max-width: 535px;
-  
-  ${theme.breakpoints.down('lg')} {
+
+  ${({ theme }) => theme.breakpoints.down('lg')}  {
     max-width: 1440px;
   },
 `;
--- src/components/Knowledge/KnowledgeInfo.tsx
+++ src/components/Knowledge/KnowledgeInfo.tsx
@@ -1,23 +1,21 @@
-import { FC } from 'react';
 import { Box, styled, Typography } from '@mui/material';
 import { IKnowledgeInfo } from './Knowledge.types';
-import theme from '../../theme';
 
-const KnowledgeInfo: FC<Omit<IKnowledgeInfo, 'id'>> = ({ image, description, statistics }) => {
+const KnowledgeInfo = ({ image, description, statistics }: Omit<IKnowledgeInfo, 'id'>) => {
   return (
-    <Box border={1} borderColor={'divider'} borderRadius={2} p={{ xs: 1, md: 2 }} minHeight={{ xs: 168, sm: 0 }}>
+    <Box border={1} borderColor="divider" borderRadius={2} p={{ xs: 1, md: 2 }} minHeight={{ xs: 168, sm: 0 }}>
       <GridContainer>
-        <Box alignItems={'center'} maxHeight={48}>
+        <Box alignItems="center" maxHeight={48}>
           {image}
         </Box>
         <Box>
-          <Typography variant={'h4'} textAlign={{ xs: 'center', lg: 'left' }}>
+          <Typography variant="h4" textAlign={{ xs: 'center', lg: 'left' }}>
             {statistics}
           </Typography>
           <Typography
-            variant={'subtitle2'}
+            variant="subtitle2"
             fontSize={{ xs: 14, lg: 16 }}
-            color={'secondary.dark'}
+            color="secondary.dark"
             textAlign={{ xs: 'center', lg: 'left' }}
           >
             {description}
@@ -30,12 +28,11 @@ const KnowledgeInfo: FC<Omit<IKnowledgeInfo, 'id'>> = ({ image, description, sta
 
 const GridContainer = styled('div')`
   display: flex;
-  flex-direction: row;
   align-items: flex-start;
   gap: 16px;
   height: 100%;
 
-  ${theme.breakpoints.down('sm')} {
+  ${({ theme }) => theme.breakpoints.down('sm')} {
     flex-direction: column;
     align-items: center;
     justify-content: center;
