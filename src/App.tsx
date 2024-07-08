1579fe861de91bd62e20afc011452972a19e613e
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-08 17:06:38 +0300

    fix(BrowserRouter): BrowserRouter вынесен из main в App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/Login/LoginPage.tsx';
 import HomePage from './components/HomePage/HomePage.tsx';
 
@@ -11,11 +11,13 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage typeLogin={'authorization'} />} />
-          <Route path="/registration" element={<LoginPage typeLogin={'registration'} />} />
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage typeLogin={'authorization'} />} />
+            <Route path="/registration" element={<LoginPage typeLogin={'registration'} />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </ThemeProvider>
   );
