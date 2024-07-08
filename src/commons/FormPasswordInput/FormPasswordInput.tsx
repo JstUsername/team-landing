538b7a8f23b387851eafaf43d0e4438cebb66087
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-08 19:16:36 +0300

    feat(FormPasswordInput): инпут для пароля вынесен из LoginPage в отдельный компонент.
    Для FormPasswordInput добавлена иконка закрытого глаза при видимом пароле.

---
+++ src/commons/FormPasswordInput/FormPasswordInput.tsx
@@ -0,0 +1,36 @@
+import FormInput from '../FormInput/FormInput.tsx';
+import EyeIcon from '../../assets/login/icons/eye.svg?react';
+import EyeCloseIcon from '../../assets/login/icons/eye-close.svg?react';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import { useState } from 'react';
+
+export default function FormPasswordInput() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <FormInput
+      inputId="password-input"
+      inputLabel="Пароль"
+      inputPlaceholder="******"
+      type={showPassword ? 'text' : 'password'}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={() => setShowPassword((show) => !show)}
+              edge="end"
+            >
+              {!showPassword ? (
+                <EyeIcon title="Change password visibility" width="20px" height="20px" />
+              ) : (
+                <EyeCloseIcon title="Change password visibility" width="20px" height="20px" />
+              )}
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+}
