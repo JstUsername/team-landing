10f4c89e7afe6fd8bf88b49ed4f2327f44d6574d
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-08 17:04:31 +0300

    fix(import, routing): исправлены импорты и пути для роутинга, AppContent заменен на HomePage
    # Conflicts:
    #	src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 
 import { Routes, Route } from 'react-router-dom';
-import Login from './components/Login/Login.tsx';
-import Header from './components/Header/Header.tsx';
+import LoginPage from './components/Login/LoginPage.tsx';
+import HomePage from './components/HomePage/HomePage.tsx';
 
 function App() {
   return (
@@ -12,20 +12,12 @@ function App() {
       <CssBaseline />
       <div className="App">
         <Routes>
-          <Route path="/" element={<AppContent />} />
-          <Route path="/login" element={<Login typeLogin={'authorization'} />} />
-          <Route path="/registration" element={<Login typeLogin={'registration'} />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage typeLogin={'authorization'} />} />
+          <Route path="/registration" element={<LoginPage typeLogin={'registration'} />} />
         </Routes>
       </div>
     </ThemeProvider>
-  );
-}
-
-function AppContent() {
-  return (
-    <div className="AppContent">
-      <Header />
-    </div>
   );
 }
 
