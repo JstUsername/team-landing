878d5e799694df45029e14da917821c7526fa089
vabulaeva <veronikabylaeva99@gmail.com>
2024-07-09 20:57:10 +0300

    fix(knowledge): Исправлено использование theme в кнопке

--- src/commons/button/ButtonLink.tsx
+++ src/commons/button/ButtonLink.tsx
@@ -1,7 +1,6 @@
 import { FC, PropsWithChildren } from 'react';
 import { ButtonProps, Link, styled } from '@mui/material';
 import { Button as MUIButton } from '@mui/material';
-import { theme } from '../../theme';
 
 const ButtonLink: FC<PropsWithChildren<ButtonProps>> = ({ children, ...rest }) => {
   return (
@@ -11,20 +10,20 @@ const ButtonLink: FC<PropsWithChildren<ButtonProps>> = ({ children, ...rest }) =
   );
 };
 
-const Button = styled(MUIButton)<ButtonProps>(`
+const Button = styled(MUIButton)<ButtonProps>`
   padding-inline: 32px;
   padding-block: 16px;
-  border: 2px solid ${theme.palette.info.main};
+  border: 2px solid ${({ theme }) => theme.palette.info.main};
   border-radius: 0;
   box-shadow: none;
   text-transform: none;
   :hover {
     border: 2px solid #3064ac;
   };
-  ${theme.breakpoints.down('sm')} {
+  ${({ theme }) => theme.breakpoints.down('sm')} {
     padding-inline: 28px;
     padding-block: 14px;
   },
-`);
+`;
 
 export default ButtonLink;
